26d0863c5ff6fced12e5c0a9d1ee6d20206796ea
slanau <silu@gft.com>
2013-01-23 08:56:08 +0100

    New Archetype Files

---
+++ tools/archetypes/gwtproject/src/main/resources/archetype-resources/__rootArtifactId__-gwtfrontend/src/main/java/gwtfrontend/common/ApplicationConstants.java
@@ -0,0 +1,7 @@
+package com.gft.storefront.gwtfrontend.common;
+
+import org.appverse.web.framework.frontend.gwt.common.FrameworkConstants;
+
+public interface ApplicationConstants extends FrameworkConstants {
+
+}
---
+++ tools/archetypes/gwtproject/src/main/resources/archetype-resources/__rootArtifactId__-gwtfrontend/src/main/java/gwtfrontend/common/ApplicationMessages.java
@@ -0,0 +1,7 @@
+package com.gft.storefront.gwtfrontend.common;
+
+import org.appverse.web.framework.frontend.gwt.common.FrameworkMessages;
+
+public interface ApplicationMessages extends FrameworkMessages {
+
+}
