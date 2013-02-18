fbe5b80651a2e8b1329eaf4bf4bcf4bc07e9cd56
slanau <silu@gft.com>
2013-02-18 11:45:01 +0100

    Refactor to sun jms dependency

--- modules/backend/core/persistence/src/main/java/org/appverse/web/framework/backend/persistence/services/integration/ELJMSTransportManager.java
+++ modules/backend/core/persistence/src/main/java/org/appverse/web/framework/backend/persistence/services/integration/ELJMSTransportManager.java
@@ -1,12 +1,12 @@
 /*
  Copyright (c) 2012 GFT Appverse, S.L., Sociedad Unipersonal.
 
- This Source Code Form is subject to the terms of the Appverse Public License 
- Version 2.0 (“APL v2.0”). If a copy of the APL was not distributed with this 
+ This Source Code Form is subject to the terms of the Appverse Public License
+ Version 2.0 (“APL v2.0”). If a copy of the APL was not distributed with this
  file, You can obtain one at http://www.appverse.mobi/licenses/apl_v2.0.pdf. [^]
 
- Redistribution and use in source and binary forms, with or without modification, 
- are permitted provided that the conditions of the AppVerse Public License v2.0 
+ Redistribution and use in source and binary forms, with or without modification,
+ are permitted provided that the conditions of the AppVerse Public License v2.0
  are met.
 
  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
@@ -17,8 +17,8 @@
  INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
  LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
  PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
- WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT(INCLUDING NEGLIGENCE OR OTHERWISE) 
- ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE 
+ WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT(INCLUDING NEGLIGENCE OR OTHERWISE)
+ ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
  POSSIBILITY OF SUCH DAMAGE.
  */
 package org.appverse.web.framework.backend.persistence.services.integration;
@@ -119,12 +119,12 @@ public class ELJMSTransportManager extends JMSTopicTransportManager {
 				rcmException = RemoteCommandManagerException
 						.errorCreatingLocalJMSConnection(config.getTopic(),
 								config.getTopicConnectionFactory(),
-								config.getContextProperties(), ex);
+								 ex);
 			} else {
 				rcmException = RemoteCommandManagerException
 						.errorCreatingJMSConnection(config.getTopic(),
 								config.getTopicConnectionFactory(),
-								config.getContextProperties(), ex);
+								 ex);
 			}
 			throw rcmException;
 		} finally {
