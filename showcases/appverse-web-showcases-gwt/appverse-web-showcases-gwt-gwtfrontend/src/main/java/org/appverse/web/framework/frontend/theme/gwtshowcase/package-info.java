ea23d826dbbfbcd67db3bcc3aa9189b76b56b305
sergi <sergi@lanau.net>
2013-03-07 20:47:34 +0100

    Syncronize

--- showcases/appverse-web-showcases-gwt/appverse-web-showcases-gwt-gwtfrontend/src/main/java/org/appverse/web/framework/frontend/theme/gwtshowcase/package-info.java
+++
@@ -1,5 +0,0 @@
-/**
- * The default gwtshowcase colored theme. This theme builds on the Base theme by applying gwtshowcase images and styling rules to
- * the GXT components.
- */
-package org.appverse.web.framework.frontend.theme.gwtshowcase;
