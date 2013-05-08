88cf88c1d9aab16711605f1b8155146e6a3fedc2
slanau <silu@gft.com>
2013-05-08 18:55:22 +0200

    Signed-off-by: slanau <silu@gft.com>

--- tools/project-generator/src/main/java/org/appverse/web/tools/projectgenerator/plugin/ProjectGeneratorMojo.java
+++ tools/project-generator/src/main/java/org/appverse/web/tools/projectgenerator/plugin/ProjectGeneratorMojo.java
@@ -7,25 +7,39 @@ import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugin.MojoFailureException;
 import org.apache.maven.plugin.logging.Log;
 import org.apache.maven.plugins.annotations.Mojo;
+import org.apache.maven.plugins.annotations.Parameter;
 
 @Mojo(name="generate-project")
 public class ProjectGeneratorMojo extends AbstractMojo {
 
+	@Parameter(property="presentationLayerType",defaultValue="GWT")
+	private String presentationLayerType;
 	public void execute() throws MojoExecutionException, MojoFailureException {
 		Log logger = getLog();
-		try {
-			Process process = Runtime.getRuntime().exec("cmd.exe /c mvn clean");
+		logger.info("Creating project...");
+		try{
+		if(PresentationLayerType.GWT.equals(presentationLayerType)){
+			logger.info("Project presentation layer flavour selected: "+PresentationLayerType.GWT.getValue());
+			Process process;
+			process = Runtime.getRuntime().exec("cmd.exe /c rm pom.xml");
+			process = Runtime.getRuntime().exec("cmd.exe /c mvn archetype:generate -DarchetypeGroupId=org.appverse.web.framework.archetypes.gwt -DarchetypeArtifactId=appverse-web-archetypes-gwt -DarchetypeVersion=1.1.0-RELEASE -DgroupId=org.organization.project -DartifactId=project -Dversion=1.0-SNAPSHOT -DnewProjectName=MyProject -DtargetDbPlatform=MysqlPlatform -DinteractiveMode=false");
 			process.waitFor();
-			//log.error(process.getOutputStream().);
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (InterruptedException e) {
-			// TODO Auto-generated catch block
+			logger.info("Project generated successfully");
+		}else if(PresentationLayerType.JSF2.equals(presentationLayerType)){
+			logger.info("Project presentation layer flavour selected: "+PresentationLayerType.JSF2.getValue());
+			Process process;
+			process = Runtime.getRuntime().exec("cmd.exe /c rm pom.xml");
+			process = Runtime.getRuntime().exec("cmd.exe /c mvn archetype:generate -DarchetypeGroupId=org.appverse.web.framework.archetypes.jsf2 -DarchetypeArtifactId=appverse-web-archetypes-jsf2 -DarchetypeVersion=1.1.0-RELEASE -DgroupId=org.organization.project -DartifactId=project -Dversion=1.0-SNAPSHOT -DnewProjectName=MyProject -DinteractiveMode=false");
+			process.waitFor();
+			logger.info("Project generated successfully");
+		}else{
+			logger.info("No valid presentation flavour selected: "+presentationLayerType+" the valids ones are "+PresentationLayerType.GWT.getValue()+" and "+PresentationLayerType.JSF2.getValue());
+		}
+
+		} catch (Exception e) {
+			logger.error("Error creating Project");
 			e.printStackTrace();
 		}
-		logger.error("Hello generate project");
-		
 	}
 
 
