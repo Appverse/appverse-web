ed51346186bee8e0d621a48a9da614ac6cbcbd20
slanau <silu@gft.com>
2013-05-09 18:28:19 +0200

    Signed-off-by: slanau <silu@gft.com>

--- tools/project-generator/src/main/java/org/appverse/web/tools/projectgenerator/plugin/ProjectGeneratorMojo.java
+++ tools/project-generator/src/main/java/org/appverse/web/tools/projectgenerator/plugin/ProjectGeneratorMojo.java
@@ -1,7 +1,5 @@
 package org.appverse.web.tools.projectgenerator.plugin;
 
-import java.io.IOException;
-
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugin.MojoFailureException;
@@ -14,23 +12,37 @@ public class ProjectGeneratorMojo extends AbstractMojo {
 
 	@Parameter(property="presentationLayerType",defaultValue="GWT")
 	private String presentationLayerType;
+	@Parameter(property="groupId",defaultValue="org.organization")
+	private String groupId;
+	@Parameter(property="artifactId",defaultValue="project")
+	private String artifactId;
+	@Parameter(property="version",defaultValue="1.0-SNAPSHOT")
+	private String version;
+	@Parameter(property="name",defaultValue="project")
+	private String name;	
+	@Parameter(property="dbPlatform",defaultValue="MySQLPlatform")
+	private String dbPlatform;
+	@Parameter(property="archetypeVersion",defaultValue="1.1.0-RELEASE")
+	private String archetypeVersion;	
 	public void execute() throws MojoExecutionException, MojoFailureException {
 		Log logger = getLog();
-		logger.info("Creating project...");
+		logger.info("Creating project using archetypeVersion "+archetypeVersion+"...");
 		try{
 		if(PresentationLayerType.GWT.equals(presentationLayerType)){
 			logger.info("Project presentation layer flavour selected: "+PresentationLayerType.GWT.getValue());
 			Process process;
-			process = Runtime.getRuntime().exec("cmd.exe /c rm pom.xml");
-			process = Runtime.getRuntime().exec("cmd.exe /c mvn archetype:generate -DarchetypeGroupId=org.appverse.web.framework.archetypes.gwt -DarchetypeArtifactId=appverse-web-archetypes-gwt -DarchetypeVersion=1.1.0-RELEASE -DgroupId=org.organization.project -DartifactId=project -Dversion=1.0-SNAPSHOT -DnewProjectName=MyProject -DtargetDbPlatform=MysqlPlatform -DinteractiveMode=false");
+			process = Runtime.getRuntime().exec("cmd.exe /c mv pom.xml pom.xml_");
+			process = Runtime.getRuntime().exec("cmd.exe /c mvn archetype:generate -DarchetypeGroupId=org.appverse.web.framework.archetypes.gwt -DarchetypeArtifactId=appverse-web-archetypes-gwt -DarchetypeVersion="+archetypeVersion+" -DgroupId="+groupId+"."+artifactId+" -DartifactId="+artifactId+" -Dversion="+version+" -DnewProjectName="+name+" -DtargetDbPlatform="+dbPlatform+" -DinteractiveMode=false");
 			process.waitFor();
+			process = Runtime.getRuntime().exec("cmd.exe /c mv pom.xml_ pom.xml");
 			logger.info("Project generated successfully");
 		}else if(PresentationLayerType.JSF2.equals(presentationLayerType)){
 			logger.info("Project presentation layer flavour selected: "+PresentationLayerType.JSF2.getValue());
 			Process process;
-			process = Runtime.getRuntime().exec("cmd.exe /c rm pom.xml");
-			process = Runtime.getRuntime().exec("cmd.exe /c mvn archetype:generate -DarchetypeGroupId=org.appverse.web.framework.archetypes.jsf2 -DarchetypeArtifactId=appverse-web-archetypes-jsf2 -DarchetypeVersion=1.1.0-RELEASE -DgroupId=org.organization.project -DartifactId=project -Dversion=1.0-SNAPSHOT -DnewProjectName=MyProject -DinteractiveMode=false");
+			process = Runtime.getRuntime().exec("cmd.exe /c mv pom.xml pom.xml_");
+			process = Runtime.getRuntime().exec("cmd.exe /c mvn archetype:generate -DarchetypeGroupId=org.appverse.web.framework.archetypes.jsf2 -DarchetypeArtifactId=appverse-web-archetypes-jsf2 -DarchetypeVersion="+archetypeVersion+" -DgroupId="+groupId+"."+artifactId+" -DartifactId="+artifactId+" -Dversion="+version+" -DnewProjectName="+name+" -DinteractiveMode=false");
 			process.waitFor();
+			process = Runtime.getRuntime().exec("cmd.exe /c mv pom.xml_ pom.xml");
 			logger.info("Project generated successfully");
 		}else{
 			logger.info("No valid presentation flavour selected: "+presentationLayerType+" the valids ones are "+PresentationLayerType.GWT.getValue()+" and "+PresentationLayerType.JSF2.getValue());
