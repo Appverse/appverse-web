a746b1fdf169cb45d1a4217c7664f28a74084823
slanau <silu@gft.com>
2013-05-14 18:12:59 +0200

    Signed-off-by: slanau <silu@gft.com>

--- tools/archetype-generator/src/main/resources/gwt/fragment/target/classes/archetype-resources/__rootArtifactId__-gwtfrontend/src/main/java/gwtfrontend/common/ApplicationMessages.java
+++
@@ -1,7 +0,0 @@
-package ${package}.gwtfrontend.common;
-
-import org.appverse.web.framework.frontend.gwt.common.FrameworkMessages;
-
-public interface ApplicationMessages extends FrameworkMessages {
-
-}
