194b0b903820e85cb5c1d4ebaf73856085eeed04
miguelfgar <miguelfgar@gmail.com>
2013-10-10 16:33:04 +0200

    Added nocache and wilcard support for url patterns in StaticContentCacheFilter.java

--- framework/modules/backend/core/api/src/main/java/org/appverse/web/framework/backend/api/helpers/request/StaticContentCacheFilter.java
+++ framework/modules/backend/core/api/src/main/java/org/appverse/web/framework/backend/api/helpers/request/StaticContentCacheFilter.java
@@ -23,12 +23,11 @@
  */
 package org.appverse.web.framework.backend.api.helpers.request;
 
-import org.appverse.web.framework.backend.api.helpers.log.AutowiredLogger;
-import org.slf4j.Logger;
-import org.springframework.web.filter.OncePerRequestFilter;
+import jodd.typeconverter.Convert;
+import jodd.typeconverter.TypeConversionException;
+import jodd.util.Wildcard;
 
-import javax.servlet.FilterChain;
-import javax.servlet.ServletException;
+import javax.servlet.*;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
@@ -42,30 +41,143 @@ import java.util.Date;
  * (you can use "strong caching" for resources that are application data if you are sure your application modifies the URL every time the data is modified).
  * GWT apps resources marked with standard ".nocache." will be bypassed.
  * This solution is based on Google recommendations: https://developers.google.com/speed/docs/best-practices/caching?hl=es
+ *
+ * Params initialization and url patterns treatment (matches, nocaches and wildcards) is inspired by jodd.servlet.filter.GzipFilter.java
+ * http://jodd.org/doc/htmlstapler/enabling-gzip.html#GZIP-filter
  */
-public class StaticContentCacheFilter extends OncePerRequestFilter {
+public class StaticContentCacheFilter implements Filter {
 
-    @AutowiredLogger
-    private static Logger logger;
+    protected String[] matches;
+    protected String[] nocaches;
+    protected boolean wildcards;
 
     @Override
-    protected void doFilterInternal(HttpServletRequest req, HttpServletResponse res, FilterChain filterChain) throws ServletException, IOException {
+    public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
         HttpServletRequest httpRequest = (HttpServletRequest) req;
         HttpServletResponse httpResponse = (HttpServletResponse) res;
-        String requestUri = httpRequest.getRequestURI();
-        if (!requestUri.contains(".nocache.")) {
-            // Setting "Expires" +1 year following Google recommendations for static content
+
+        if (isNoCacheable(httpRequest) == true){
+            // We do not cache non-cacheable GWT resources
+            httpResponse.addHeader("Cache-Control", "no-cache");
+        }else if (isCacheableEligible(httpRequest) == true) {
+            // Is cacheable
             Calendar calendar = Calendar.getInstance();
             calendar.add(Calendar.YEAR, 1);
             Date date = calendar.getTime();
             SimpleDateFormat df = new SimpleDateFormat("EEE, dd MMM yyyy HH:mm:ss z");
             httpResponse.addHeader("Expires", df.format(date));
-            logger.debug("Added Expires:" + df.format(date)  + " header to request. Request URI: " + requestUri);
-        } else {
-            // We do not cache non-cacheable GWT resources
-            httpResponse.addHeader("Cache-Control", "no-cache");
-            logger.debug("Added Cache-Control:no-cache header to request. Request URI: " + requestUri);
         }
-        filterChain.doFilter(req, res);
+        else if (isCacheableEligible(httpRequest) == true) {
+            // We do not mofidy the response
+        }
+        chain.doFilter(httpRequest, httpResponse);
     }
+
+    /**
+     * Filter initialization.
+     */
+    @Override
+    public void init(FilterConfig config) throws ServletException {
+
+        try {
+            wildcards = Convert.toBooleanValue(config.getInitParameter("wildcards"), false);
+        } catch (TypeConversionException ignore) {
+            wildcards = false;
+        }
+
+        // match string
+        String uriMatch = config.getInitParameter("match");
+
+        if ((uriMatch != null) && (uriMatch.equals("*") == false)) {
+            matches = uriMatch.split(",");
+            for (int i = 0; i < matches.length; i++) {
+                matches[i] = matches[i].trim();
+            }
+        }
+
+        // exclude string
+        String uriNoCache = config.getInitParameter("nocache");
+
+        if (uriNoCache != null) {
+            nocaches = uriNoCache.split(",");
+            for (int i = 0; i < nocaches.length; i++) {
+                nocaches[i] = nocaches[i].trim();
+            }
+        }
+    }
+
+    public void destroy() {
+        matches = null;
+        nocaches = null;
+    }
+
+    /**
+     * Determine if uri is eligible for caching
+     */
+    @Override
+    private boolean isCacheableEligible(HttpServletRequest req) {
+        String uri = req.getRequestURI();
+        if (uri == null) {
+            return false;
+        }
+
+        boolean result = false;
+
+        if (matches == null) {							// match=*
+            result = true;
+        } else {
+            if (wildcards) {
+                result = Wildcard.matchPathOne(uri, matches) != -1;
+            } else {
+                for (String match : matches) {
+                    if (uri.contains(match)) {
+                        result = true;
+                        break;
+                    }
+                }
+            }
+        }
+
+        if ((result == true) && (nocaches != null)) {
+            if (wildcards) {
+                if (Wildcard.matchPathOne(uri, nocaches) != -1) {
+                    result = false;
+                }
+            } else {
+                for (String nocache : nocaches) {
+                    if (uri.contains(nocache)) {
+                        result = false;						// nocaches found
+                        break;
+                    }
+                }
+            }
+        }
+        return result;
+    }
+
+    private boolean isNoCacheable(HttpServletRequest req){
+        String uri = req.getRequestURI();
+        if (uri == null) {
+            return false;
+        }
+
+        boolean result = false;
+
+        if (nocaches != null) {
+            if (wildcards) {
+                if (Wildcard.matchPathOne(uri, nocaches) != -1) {
+                    result = true;
+                }
+            } else {
+                for (String nocache : nocaches) {
+                    if (uri.contains(nocache)) {
+                        result = true;						// nocaches found
+                        break;
+                    }
+                }
+            }
+        }
+        return result;
+    }
+
 }
