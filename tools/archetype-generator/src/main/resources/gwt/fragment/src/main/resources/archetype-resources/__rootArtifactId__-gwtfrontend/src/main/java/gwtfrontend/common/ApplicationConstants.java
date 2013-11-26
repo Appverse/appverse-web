6f76c8ae7b42398b80ea1e87e6b7f1895aac6161
Carlos Rubio <carlos.rubio@gft.com>
2013-11-26 13:13:44 +0100

    Showcases and tools do not live here any longer

--- tools/archetype-generator/src/main/resources/gwt/fragment/src/main/resources/archetype-resources/__rootArtifactId__-gwtfrontend/src/main/java/gwtfrontend/common/ApplicationConstants.java
+++
@@ -1,7 +0,0 @@
-package ${package}.gwtfrontend.common;
-
-import org.appverse.web.framework.frontend.gwt.common.FrameworkConstants;
-
-public interface ApplicationConstants extends FrameworkConstants {
-
-}
