e1a50b69f8939699d2e7f4f15097bd5aff9b0c4b
miguelfgar <miguelfgar@gmail.com>
2014-01-30 15:44:21 +0100

    Removed old gwt archetype

--- framework/archetypes/gwt/src/main/resources/archetype-resources/__rootArtifactId__-gwtfrontend/src/main/java/gwtfrontend/main/MainConstants.java
+++
@@ -1,9 +0,0 @@
-package ${package}.gwtfrontend.main;
-
-import ${package}.gwtfrontend.common.ApplicationConstants;
-
-public interface MainConstants extends ApplicationConstants {
-
-	// Add here just module specific constants
-
-}
