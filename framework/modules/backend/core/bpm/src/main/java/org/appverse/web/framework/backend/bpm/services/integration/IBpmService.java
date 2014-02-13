d16b2e2c68c59d8aec270042c321020b3e476d5f
Roger Bofarull <roger.bofarull@gft.com>
2014-02-13 12:39:34 +0100

    bpm module created

---
+++ framework/modules/backend/core/bpm/src/main/java/org/appverse/web/framework/backend/bpm/services/integration/IBpmService.java
@@ -0,0 +1,11 @@
+package org.appverse.web.framework.backend.bpm.services.integration;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: RRBL
+ * Date: 13/02/14
+ * Time: 9:55
+ * To change this template use File | Settings | File Templates.
+ */
+public interface IBpmService {
+}
