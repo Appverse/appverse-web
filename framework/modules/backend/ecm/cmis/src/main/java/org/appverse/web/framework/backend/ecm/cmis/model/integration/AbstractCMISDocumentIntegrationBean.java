70f8540d0cc039e3c927ab26cfeb058c94251aac
Miguel Fernandez <miguel.fernandez@gft.com>
2014-05-19 17:12:37 +0200

    ECM Integration (work in progress)

---
+++ framework/modules/backend/ecm/cmis/src/main/java/org/appverse/web/framework/backend/ecm/cmis/model/integration/AbstractCMISDocumentIntegrationBean.java
@@ -0,0 +1,10 @@
+package org.appverse.web.framework.backend.ecm.cmis.model.integration;
+
+import org.appverse.web.framework.backend.ecm.core.model.integration.AbstractDocumentIntegrationBean;
+
+public class AbstractCMISDocumentIntegrationBean extends AbstractDocumentIntegrationBean{
+
+    // Add here all Document properties supported by CMIS 1.0 and 1.1
+
+
+}
