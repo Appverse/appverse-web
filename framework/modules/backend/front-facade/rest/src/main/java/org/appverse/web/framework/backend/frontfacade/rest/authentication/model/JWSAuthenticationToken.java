5a520d85e3a35d1919c25c43417aed6d30c32d18
Marc Rodriguez-Estivill <mrodriguezestivill@gmail.com>
2014-05-19 17:14:28 +0200

    JWS Authentication Classes

---
+++ framework/modules/backend/front-facade/rest/src/main/java/org/appverse/web/framework/backend/frontfacade/rest/authentication/model/JWSAuthenticationToken.java
@@ -0,0 +1,86 @@
+package org.appverse.web.framework.backend.frontfacade.rest.authentication.model;
+
+import org.springframework.security.authentication.AbstractAuthenticationToken;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.SpringSecurityCoreVersion;
+
+import java.util.Collection;
+
+/**
+* An {@link org.springframework.security.core.Authentication} implementation that is designed for simple presentation
+* of a username and password.
+* <p>
+* The <code>principal</code> and <code>credentials</code> should be set with an <code>Object</code> that provides
+* the respective property via its <code>Object.toString()</code> method. The simplest such <code>Object</code> to use
+* is <code>String</code>.
+*
+* @author Ben Alex
+*/
+public class JWSAuthenticationToken extends AbstractAuthenticationToken {
+
+	private static final long serialVersionUID = SpringSecurityCoreVersion.SERIAL_VERSION_UID;
+
+	//~ Instance fields ================================================================================================
+
+	private final Object principal;
+
+	private final String jwsToken;
+
+	//~ Constructors ===================================================================================================
+
+	/**
+	 * This constructor can be safely used by any code that wishes to create a
+	 * <code>UsernamePasswordAuthenticationToken</code>, as the {@link
+	 * #isAuthenticated()} will return <code>false</code>.
+	 *
+	 */
+	public JWSAuthenticationToken(final String token) {
+		super(null);
+		this.principal = null;
+		this.jwsToken = token;
+	}
+
+	/**
+	 * This constructor should only be used by <code>AuthenticationManager</code> or <code>AuthenticationProvider</code>
+	 * implementations that are satisfied with producing a trusted (i.e. {@link #isAuthenticated()} = <code>true</code>)
+	 * authentication token.
+	 *
+	 * @param principal
+	 * @param authorities
+	 */
+	public JWSAuthenticationToken(final Object principal,
+			final Collection<? extends GrantedAuthority> authorities) {
+		super(authorities);
+		this.principal = principal;
+		this.jwsToken = null;
+		super.setAuthenticated(true); // must use super, as we override
+	}
+
+	//~ Methods ========================================================================================================
+
+	@Override
+	public Object getPrincipal() {
+		return this.principal;
+	}
+
+	@Override
+	public void setAuthenticated(final boolean isAuthenticated) throws IllegalArgumentException {
+		if (isAuthenticated) {
+			throw new IllegalArgumentException(
+					"Cannot set this token to trusted - use constructor which takes a GrantedAuthority list instead");
+		}
+
+		super.setAuthenticated(false);
+	}
+
+	@Override
+	public Object getCredentials() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public String getJwsToken() {
+		return jwsToken;
+	}
+
+}
