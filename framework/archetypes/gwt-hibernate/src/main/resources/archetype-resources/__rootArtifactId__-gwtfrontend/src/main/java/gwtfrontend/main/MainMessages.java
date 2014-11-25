c7c63e43b1bbfe68d1e8fdd9277d77c01dad8b0b
Miguel Fernandez <miguel.fernandez@gft.com>
2014-11-25 16:34:20 +0100

    Removed Archetypes from Appverse Web project structure as they have been moved to an own project: https://github.com/Appverse/appverse-web/tree/master/framework/archetypes

--- framework/archetypes/gwt-hibernate/src/main/resources/archetype-resources/__rootArtifactId__-gwtfrontend/src/main/java/gwtfrontend/main/MainMessages.java
+++
@@ -1,9 +0,0 @@
-package ${package}.gwtfrontend.main;
-
-import ${package}.gwtfrontend.common.ApplicationMessages;
-import com.google.gwt.i18n.client.impl.plurals.DefaultRule_en;
-
-public interface MainMessages extends ApplicationMessages {
-
-
-}
